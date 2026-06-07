b82cafefe48527161601a69e1c25a7343982e7f2
Shaheen <s.shaheen@crickslab.com>
2026-06-07 23:02:22 +0500

    add the edit category modal

--- src/app/dashboard/categories/components/table-actions.tsx
+++ src/app/dashboard/categories/components/table-actions.tsx
@@ -1,50 +1,54 @@
-'use client'
+"use client";
 
-import { Button } from "@/components/ui/button"
-import { deleteCategory } from "../../server-actions/category-actions"
-import { toast } from "sonner"
-import { Category } from "@prisma/client"
-import Link from "next/link"
-import { Edit, Trash2 } from "lucide-react"
-
+import { Button } from "@/components/ui/button";
+import { WarnAlertDialog } from "@/components/warn-alert-dialog";
+import { deleteCategory } from "../../server-actions/category-actions";
+import { toast } from "sonner";
+import { Category } from "@prisma/client";
+import { Edit, Trash2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { EditCategoryModal } from "./edit-category-modal";
 
 interface Props {
   category: Category;
 }
 
 export const TableActions = ({ category }: Props) => {
+  const router = useRouter();
 
-
-  const handleDelete = async (id: string) => {
-
-    const response = await deleteCategory(id);
+  const handleDelete = async () => {
+    const response = await deleteCategory(category.id);
 
     if (response.success) {
-      toast.success("Category deleted successfully")
+      toast.success(response.message);
+      router.refresh();
     } else {
-      toast.error("Category deleted successfully")
+      toast.error(response.message);
     }
-  }
+  };
 
+  return (
+    <div>
+      <EditCategoryModal
+        category={category}
+        trigger={
+          <Button variant="outline" size="sm" className="mr-2">
+            <Edit className="h-4 w-4 mr-2" /> Edit
+          </Button>
+        }
+      />
 
-  return <div>
-    <Link href={`/dashboard/categories/${category.id}/edit`}>
-      <Button
-        variant="outline"
-        size="sm"
-        className="mr-2"
-      >
-        <Edit className="h-4 w-4 mr-2" /> Edit
-      </Button>
-    </Link>
-
-
-    <Button
-      variant="destructive"
-      size="sm"
-      onClick={() => handleDelete(category.id)}
-    >
-      <Trash2 className="h-4 w-4" />
-    </Button>
-  </div>
-} 
+      <WarnAlertDialog
+        title="Delete category?"
+        description={`Do you really want to delete "${category.name}"? This action cannot be undone.`}
+        confirmBtnTitle="Delete"
+        triggerElement={
+          <Button variant="destructive" size="sm">
+            <Trash2 className="h-4 w-4" />
+          </Button>
+        }
+        onConfirm={handleDelete}
+      />
+    </div>
+  );
+};
